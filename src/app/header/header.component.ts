01a25c462af3aeb4f1818fdd3b734a2fc911c568
Tommy Han <tommy@Tommys-Air.home>
2020-05-31 23:13:23 -0400

    added header to html

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -2,13 +2,14 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'header',
-  template: `
-    <h1>
-      {{ title }}
-    </h1>
-    <p>Get it done today</p>
-    <p>No excuses</p>
-  `
+  templateUrl: './header.component.html'
+  // template: `
+  //   <h1>
+  //     {{ title }}
+  //   </h1>
+  //   <p>Get it done today</p>
+  //   <p>No excuses</p>
+  // `
 })
 export class HeaderComponent {
   title = 'Angular Shopping List';
